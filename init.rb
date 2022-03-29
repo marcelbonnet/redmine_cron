0c8bea3a1de1acfc6858f81a5931f5390258c27b
Marcel Bonnet <marcelbonnet@anatel.gov.br>
2022-03-29 08:58:58 -0300

    Remove broken link to plugin settings

--- init.rb
+++ init.rb
@@ -4,14 +4,12 @@ require 'redmine'
 Redmine::Plugin.register :redmine_cron do
   name 'Redmine job scheduler'
   description ''
-  version '1.0.2'
+  version '1.0.3'
   author_url 'https://github.com/marcelbonnet/'
   url 'https://github.com/marcelbonnet/redmine_cron'
   author 'Marcel Bonnet'
 
   requires_redmine :version_or_higher => '3.0.0'
-
-  settings partial: 'settings/redmine_cron.html'
 
   menu :admin_menu, :cron_tasks,
     { :controller => 'cron_tasks', :action => 'index' },
